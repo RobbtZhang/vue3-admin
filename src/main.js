1ada21eb0047d7d983a7d317e89b24e0df1fa6bb
zhangyingsheng <652057507@qq.com>
2021-12-13 23:20:01 +0800

    feat: 用户管理列表展示

--- src/main.js
+++ src/main.js
@@ -11,8 +11,10 @@ import installIcons from '@/icons/index.js'
 import i18n from '@/i18n'
 
 import './permission'
+import installFilter from '@/filters'
 
 const app = createApp(App)
+installFilter(app)
 installElementPlus(app)
 installIcons(app)
 app.use(store).use(router).use(i18n).mount('#app')
